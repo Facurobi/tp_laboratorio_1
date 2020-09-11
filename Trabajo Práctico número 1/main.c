cafcbdf88b71cbf55925857bf2cf523fc01048ce
Facurobi <63026137+Facurobi@users.noreply.github.com>
2020-09-11 08:55:28 -0300

    Add files via upload

---
+++ Trabajo Práctico número 1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Funciones.h"
+#include "Funciones.c"
+int main()
+{
+    int primerNumero = 0;
+    int segundoNumero = 0;
+
+    do
+        {
+
+
+
+
+        }while();
+
+
+
+
+    return 0;
+}
