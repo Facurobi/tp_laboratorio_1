524d2db934119f970e9ba80f13face4432372d5c
Facurobi <63026137+Facurobi@users.noreply.github.com>
2020-09-11 09:11:05 -0300

    Add files via upload

--- Trabajo Práctico número 1/Funciones.h
+++ Trabajo Práctico número 1/Funciones.h
@@ -1,1 +1,11 @@
+//HEADERS --- Planteo de las funciones
 
+int suma (int a, int b); // Se suman los dos numeros ingresados.
+
+int resta (int a, int b); // Se restan los dos numeros ingresados.
+
+int multiplicacion (int a, int b); // Se multiplican los 2 numeros ingresados.
+
+float division (int a, int b); // Se dividen los 2 numeros ingresados.
+
+int factorial(int a); // El producto de todos los numeros positivos menores o iguales al numero ingresado.
