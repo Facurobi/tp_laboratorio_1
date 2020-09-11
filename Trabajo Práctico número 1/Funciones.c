8abff778881e8c6b5c2e0f0e78bbbc49cf6c6de3
Facurobi <63026137+Facurobi@users.noreply.github.com>
2020-09-11 09:16:06 -0300

    Add files via upload

--- Trabajo Práctico número 1/Funciones.c
+++ Trabajo Práctico número 1/Funciones.c
@@ -1,3 +1,5 @@
+// Aca expresamos el desarrollo de las funciones utilizadas en Funciones.h
+
 #include "Funciones.h"
 
 int suma (int a, int b)
@@ -17,3 +19,27 @@ int suma (int a, int b)
  * return devuelve el resultado de la suma de los dos numeros enteros
  *
  */
+
+ int resta (int a, int b)
+{
+ int resultado;
+ resultado  = a - b;
+
+ return resultado;
+}
+
+
+ /**  Esta funcion resta los dos enteros ingresados y devuelve el resultado
+ *
+ * int a recibe un numero para restar
+ *
+ * int b recibe un numero para restar
+ *
+ * return devuelve el resultado de la resta de los dos numeros enteros
+ *
+ */
+
+
+
+
+
